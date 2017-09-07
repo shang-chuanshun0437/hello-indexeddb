7a3c59071bd277fc4a4ab63695f8298363775d33
ttang <tison.tang@morningstar.com>
2017-09-07 16:55:21 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,135 @@
 # indexdb.js
+
 A class packaging indexedDB by javascript, use for webapp localstorage.
+
+## Usage
+
+```
+let db = new IndexDB({
+  name: 'mydb', // required
+  version: 1, // optional, the beginning of version, read more from `.use()`
+  storeName: 'mystore', // required
+  primaryKey: 'id', // required
+  indexes: [ // optional
+    {
+      name: 'id',
+      key: 'id', // omissible, if not exists, name will be used as key
+      unique: true, // optional
+    },
+    {
+      name: 'std_name',
+    },
+  ],
+})
+db.add({
+  id: 1001,
+  std_name: 'GoFei',
+})
+db.add({
+  id: 1002,
+  std_name: 'Yolanda',
+})
+
+db.get(1002).then(std => alert(`Name of this student is ${std.std_name}`))
+```
+
+## options
+
+When you new the class, you should pass options:
+
+### name
+
+The name of a new indexedDB database instance. You can see it in your browser develop dashboard.
+
+### version
+
+The version of this instance. If you do not pass one, 1 will be used. Read more from `.use` method.
+
+### storeName
+
+Like tables in SQL database, a objectStore in an indexedDB should have a name.
+
+### primaryKey
+
+The primaryKey of every object you put into objectStore.
+
+### indexes
+
+array, contains:
+
+  - name: the name of this index
+  - key: the keyPath to use, choose a property of objects those you want to put into
+  - unique: whether make the index unique
+
+## methods
+
+All methods return a Promise instance.
+
+### get(key)
+
+Get a object from indexedDB by its primaryKey.
+
+### query(indexName, value)
+
+Get a object by one name of its indexes and certain value. i.e.
+
+```
+db.query('std_name', 'GoFei').then(item => console.log(item))
+```
+
+Only the first item will be returned even there are several std_name=GoFei in your database.
+
+### all()
+
+Get all records from your objectStore.
+
+### add(item)
+
+Append a object into your database. Notice, your item's properties should contain primaryKey.
+
+### put(item)
+
+Update a object in your database. Notice, your item's properties should contain primaryKey. Or inserts a new record if the given item does not already exist.
+
+### del(key)
+
+Delete a object by its primaryKey.
+
+### close()
+
+Close current connect.
+
+### use({storeName, primaryKey, indexes})
+
+Change objectStore.
+
+1> switch to a exists objectStore, only pass `storeName`.
+
+2> modify current objectStore indexes, use it, pass `storeName` and `indexes`. Notice: primaryKey can NOT be modified. And db.version will be increased 1.
+
+2> add a new objectStore, use it, pass `storeName` and `primaryKey` and `indexes(optional)`. After created, objectStore will be switched to be new objectStore, and db.version will be increased 1.
+
+### $store()
+
+Get current objectStore, you should know the original [indexedDB operation](https://developer.mozilla.org/en-US/docs/Web/API/IDBObjectStore).
+
+```
+db.$store().then(objectStore => {
+  let request = objectStore.get(1001)
+  request.onsuccess = e => {
+    ...
+  }
+})
+```
+
+## Extends
+
+Because this library is written to be a ES6 class, so you can easliy extend it:
+
+```
+class YourDB extends IndexDB {
+  clear() {
+    this.$store().then(objectStore => objectStore.clear())
+  }
+}
+```
---
+++ indexdb.js
@@ -0,0 +1,232 @@
+class IndexDB {
+	constructor(options) {
+		this.options = options
+		this.name = options.name
+		this.version = options.version || 1
+		this.storeName = ''
+		this.stores = {}
+
+		this.use({options.storeName, options.primaryKey, options.indexes})
+		this.connect(options)
+	}
+	connect() {
+		return new Promise((resolve, reject) => {
+			let {name, version, storeName} = this
+			let storage = this.stores[storeName]
+			let {primaryKey, indexes} = storage
+			let request = window.indexedDB.open(name, version)
+			request.onupgradeneeded = e => {
+				let db = e.target.result
+
+				// get objectStore
+				let objectStore = null
+				if (db.objectStoreNames.contains(storeName)) {
+					objectStore = e.target.transaction.objectStore(storeName)
+				}
+				else {
+					objectStore = db.createObjectStore(storeName, {keyPath: primaryKey})
+				}
+
+				// it's not able to modify primaryKey
+
+				// modify indexes
+				if (indexes) {
+					let indexNames = objectStore.indexNames
+					indexes.forEach(index => {
+						if (indexNames.contains(index.name)) {
+							objectStore.deleteIndex(index.name)
+						}
+						objectStore.createIndex(index.name, index.key || index.name, {unique: index.unique})
+					})
+				}
+			}
+			request.onerror = e => {
+				reject(e)
+			}
+			request.onsuccess = e => {
+				resolve(e.target.result)
+			}
+		})
+	}
+	use({storeName, primaryKey, indexes}) {
+		let stores = this.stores
+		let objectStore = stores[storeName]
+
+		// when create a new objectStore and primaryKey not given
+		if (!objectStore && !primaryKey) {
+			throw new Error('primaryKey required!')
+			return
+		}
+		// when only switch to a exists objectStore
+		if (objectStore && !primaryKey && !indexes) {
+			this.storeName = storeName
+			return
+		}
+		// when modify a exists objectStore and want to modify primaryKey
+		if (objectStore && primaryKey) {
+			throw new Error('primaryKey can not be modified!')
+			return
+		}
+
+		this.storeName = storeName
+		this.version ++
+
+		objectStore = stores[storeName] = stores[storeName] || {}
+		objectStore.name = storeName
+		objectStore.version = this.version
+		if (primaryKey) {
+			objectStore.primaryKey = primaryKey
+		}
+		if (indexes) {
+			objectStore.indexes = indexes
+		}
+	}
+	$store() {
+		return new Promise((resolve, reject) => {
+			this.connect().then(db => {
+				let storeName = this.storeName
+				let objectStore = db.transaction([storeName], 'readonly').objectStore(storeName)
+				resolve(objectStore)
+			})
+			.catch(e => {
+				reject(e)
+			})
+		})
+	}
+	get(key) {
+		return new Promise((resolve, reject) => {
+			this.connect().then(db => {
+				let storeName = this.storeName
+				let objectStore = db.transaction([storeName], 'readonly').objectStore(storeName)
+				let request = objectStore.get(key)
+				request.onsuccess = e => {
+					resolve(e.target.result)
+				}
+				request.onerror = e => {
+					reject(e)
+				}
+			})
+			.catch(e => {
+				reject(e)
+			})
+		})
+	}
+	query(indexName, value) {
+		return Promise((resolve, reject) => {
+			this.connect().then(db => {
+				let storeName = this.storeName
+				let objectStore = db.transaction([storeName], 'readonly').objectStore(storeName)
+				let objectIndex = objectStore.index(indexName)
+				let request = objectIndex.get(value)
+				request.onsuccess = e => {
+					resolve(e.target.result)
+				}
+				request.onerror = e => {
+					reject(e)
+				}
+			})
+			.catch(e => {
+				reject(e)
+			})
+		})
+	}
+	add(item) {
+		return new Promise((resolve, reject) => {
+			let primaryKey = this.primaryKey
+			let key = item[primaryKey]
+			if (!key) {
+				reject(new Error('primaryKey required!'))
+				return
+			}
+
+			this.connect().then(db => {
+				let storeName = this.storeName
+				let objectStore = db.transaction([storeName], 'readwrite').objectStore(storeName)
+				let request = objectStore.add(item)
+				request.onsuccess = e => {
+					resolve(e.target.result)
+				}
+				request.onerror = e => {
+					reject(e)
+				}
+			})
+			.catch(e => {
+				reject(e)
+			})
+		})
+	}
+	del(key) {
+		return new Promise((resolve, reject) => {
+			this.connect().then(db => {
+				let storeName = this.storeName
+				let objectStore = db.transaction([storeName], 'readwrite').objectStore(storeName)
+				let request = objectStore.delete(key)
+				request.onsuccess = e => {
+					resolve(e.target.result)
+				}
+				request.onerror = e => {
+					reject(e)
+				}
+			})
+			.catch(e => {
+				reject(e)
+			})
+		})
+	}
+	put(item) {
+		return new Promise((resolve, reject) => {
+			let primaryKey = this.primaryKey
+			let key = item[primaryKey]
+			if (!key) {
+				reject(new Error('primaryKey required!'))
+				return
+			}
+
+			this.connect().then(db => {
+				let storeName = this.storeName
+				let objectStore = db.transaction([storeName], 'readwrite').objectStore(storeName)
+				let request = objectStore.put(item)
+				request.onsuccess = e => {
+					resolve(e.target.result)
+				}
+				request.onerror = e => {
+					reject(e)
+				}
+			})
+			.catch(e => {
+				reject(e)
+			})
+		})
+	}
+	all() {
+		return new Promise((resolve, reject) => {
+			this.connect().then(db => {
+				let storeName = this.storeName
+				let objectStore = db.transaction([storeName], 'readonly').objectStore(storeName)
+				let request = objectStore.openCursor()
+				let results = []
+				request.onsuccess = e => {
+					let cursor = e.target.result
+					if (cursor) {
+						results.push(cursor.value)
+						cursor.continue()
+					}
+					else {
+						resolve(results)
+					}
+				}
+				request.onerror = e => {
+					reject(e)
+				}
+			})
+			.catch(e => {
+				reject(e)
+			})
+		})
+	}
+	close() {
+		this.connect().then(({db}) => {
+			db.close()
+		})
+	}
+}
